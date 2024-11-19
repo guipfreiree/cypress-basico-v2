7a10f3eb0ad8d1f42cf891564de44aa4cdbdc192
Guilherme Freire <guilherme.freire@altruist.com>
2024-11-19 13:06:22 -0300

    Fix the test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,7 +8,7 @@ beforeEach(() => {
 
 describe('Central de Atendimento ao Cliente TAT', () => {
     it('verifica o título da aplicação', () => {
-        cy.title().should('include', 'Central de Atendimento ao Cliente TATy');
+        cy.title().should('include', 'Central de Atendimento ao Cliente TAT');
     })
 
     it('preenche os campos obrigatórios e envia o formulário', () => {
